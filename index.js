bb0e87b3deb85a30417360965d91669cada6d456
Ravi Yadav <raviyadav2048@gmail.com>
2023-02-18 22:21:36 +0530

    fixed image issue

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const postsRoutes = require('./routes/posts');
 const likesRoutes = require('./routes/likes');
 const commentsRoutes = require('./routes/comments');
 const relationshipRoutes = require('./routes/relationship');
-
+const multer  = require('multer');
 const config = require('./config');
 require('dotenv').config();
 
@@ -19,13 +19,12 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
     next();
 })
-app.use(express.json({limit: '30mb'}));
-app.use(express.urlencoded({limit: '30mb'}))
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: '10mb'}))
 app.use(cors({
     origin: config.REACT_APP_URL
 }));
 app.use(cookieParser());
-
 
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
@@ -33,6 +32,24 @@ app.use("/posts", postsRoutes);
 app.use("/likes", likesRoutes);
 app.use("/comments", commentsRoutes);
 app.use("/relations", relationshipRoutes);
+
+// uploading files on disk using multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    },
+});
+const upload = multer({ storage: storage });
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+});
+// get static images
+app.use("/files", express.static('uploads'));
 
 const port = config.PORT;
 app.listen(port, () => {
